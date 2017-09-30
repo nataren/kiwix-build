fe23a50e3998cd361ce0621960d1247555e03cf8
César López-Natarén <cesar@nataren.io>
2017-09-30 15:47:43 -0700

    Keep reference file

---
+++ build-kiwix-arm64.sh
@@ -0,0 +1,7 @@
+export VERSION_CODE=1172710; export CONTENT_VERSION_CODE=117271; export VERSION_NAME=2017-08; python ./kiwix-build.py --target-platform android_arm --android-custom-app wikimedes kiwix-android-custom --zim-file-url https://download.kiwix.org/zim/wikipedia/wikipedia_es_medicine_novid_2017-08.zim --zim-file-size 290300506
+
+export VERSION_CODE=1172710; export CONTENT_VERSION_CODE=117271; export VERSION_NAME=2017-08; python ./kiwix-build.py --target-platform android_arm --android-custom-app wikimeden kiwix-android-custom --zim-file-url https://download.kiwix.org/zim/wikipedia/wikipedia_en_medicine_novid_2017-08.zim --zim-file-size 1181464899
+
+export VERSION_CODE=1172710; export CONTENT_VERSION_CODE=117271; export VERSION_NAME=2017-08; python ./kiwix-build.py --target-platform android_arm --android-custom-app wikimedar kiwix-android-custom --zim-file-url https://download.kiwix.org/zim/wikipedia/wikipedia_ar_medicine_novid_2017-08.zim  --zim-file-size 314283535
+
+export VERSION_CODE=1172710; export CONTENT_VERSION_CODE=117271; export VERSION_NAME=2017-08; python ./kiwix-build.py --target-platform android_arm --android-custom-app wikimedfr kiwix-android-custom --zim-file-url https://download.kiwix.org/zim/wikipedia/wikipedia_fr_medicine_novid_2017-08.zim  --zim-file-size 292409222
